ed8bdf159008c5e5dba1e0c9a23d7bbc2bdeaf27
rudranshm <102405143+rudranshm@users.noreply.github.com>
2022-04-05 01:11:40 +0530

    Update and rename Assignment1.py to Code and Documents/Python/Assignment1.py

---
+++ Code and Documents/Python/Assignment1.py
@@ -0,0 +1,12 @@
+interest=0.12 #Setting interest at 12 %
+dueamount=20000 # Setting original amount due as 20,000
+
+dueamount=dueamount+dueamount*interest # Amount due at end of first year after adding interest
+
+dueamount=dueamount - 8400 #First payment
+
+dueamount=dueamount+dueamount*interest # Amount due at end of second year, after making first payment and subsequently adding interest
+
+dueamount=dueamount - 9680 #Second payment
+
+print("The amount of loan outstanding at the end of year two is ", dueamount," rupees." )
